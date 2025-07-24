3706f86c55e34259e6dacd2abc59b759c0a850a2
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-07-24 19:02:35 +0100

    added the journey route

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+const CompanionSession = () => {
+  return <div>CompanionSession</div>;
+};
 
-export default page
+export default CompanionSession;
