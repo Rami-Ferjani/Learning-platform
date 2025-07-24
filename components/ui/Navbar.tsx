e8c31d854ec0bbf1e34d36f7fdf04ebebd2a083b
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-07-24 19:20:13 +0100

    added navbar code

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 const Navbar = () => {
   return (
-    <nav className='navbar'>
-  )
-}
+    <nav className="navbar">
+      <Link href="/">
+        <div className="flex items-center gap-2 5 cursor-pointer">
+          <Image
+            src="/image/logo.svg"
+            alt="logo"
+            width={46}
+            height={44}
+          ></Image>
+        </div>
+      </Link>{" "}
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
