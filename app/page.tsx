754add4b30e86de50de4b7add48280f896ad47a7
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-07-31 20:04:34 +0100

    added the companion card

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,42 @@
 import CompanionCard from "@/components/CompanionCard";
-import { Button } from "@/components/ui/button";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 import React from "react";
 
 const Page = () => {
   return (
     <main>
       <h1 className="text-2xl underline">Popular Companions</h1>
-      <section className="Home-section">
-        <CompanionCard />
-        <CompanionCard />
-        <CompanionCard />
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Courtsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="science"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary BUilder"
+          topic="Language"
+          subject="English Literature"
+          duration={30}
+          color="#BDE7FF"
+        />
+      
+      </section>
+      <section className="home-section">
+        <CompanionsList />
+        <CTA />
       </section>
     </main>
   );
--- components/CompanionCard.tsx
+++ components/CompanionCard.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
-const CompanionCard = () => {
-  return (
-    <div>CompanionCard</div>
-  )
+import React from "react";
+interface CompanionCardProps{
+    id:string;
+    name:string;
+    topic:string;
+    subject:string;
+    duration:number;
+    color:string;
+    
 }
+const CompanionCard = ({
+  id,
+  name,
+  topic,
+  subject,
+  duration,
+  color,
+}: CompanionCardProps) => {
+  return <div>CompanionCard</div>;
+};
 
-export default CompanionCard
+export default CompanionCard;
