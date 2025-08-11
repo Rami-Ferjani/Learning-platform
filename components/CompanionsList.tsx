7fc61de04282f3599deceede1628e140b71225ce
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-08-11 20:30:58 +0100

    makes the recent session show dynamicaly

--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -8,32 +8,37 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-interface CompanionsListProps{
-  title:string;
-  companions?:Companion[];
-  classNames?:string;
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+interface CompanionsListProps {
+  title: string;
+  companions?: Companion[];
+  classNames?: string;
 }
-const CompanionsList = ({title,companions,classNames }:CompanionsListProps) => {
+const CompanionsList = ({
+  title,
+  companions,
+  classNames,
+}: CompanionsListProps) => {
   return (
-    <article>
-      <h2>Recent Sessions</h2>
+    <article /*className={cn( ... inputs:'companion-list',classNames)}*/>
+      <h2 className="font-bold text-3xl">Recent Sessions</h2>
       <Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">Invoice</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className="text-right">Amount</TableHead>
+            <TableHead className="text-lg w-2/3">Lessons</TableHead>
+            <TableHead className="text-lg">Subject</TableHead>
+            <TableHead className="text-lg text-right">Duration</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell className="text-right">$250.00</TableCell>
-          </TableRow>
+          {companions?.map(({ id, subject, name, topic, duration }) => (
+            <TableRow key={id}>
+              <TableCell>
+                <Link href={`/companions/${id}`}>{subject}</Link>
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </article>
