9d491f24f442e65de85060350c5a6fb27a3c411b
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-09-27 10:36:32 +0100

    finished clerk signin/signup

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,11 @@
 import { SignIn } from "@clerk/nextjs";
 
 const page = () => {
-  return <SignIn />;
+  return (
+    <main className="felx items-center justify-center">
+      <SignIn />
+    </main>
+  );
 };
 
 export default page;
