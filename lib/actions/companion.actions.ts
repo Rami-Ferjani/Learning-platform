d9688dd82c03ef5e846794bd2763a6b9ddcda244
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-10-05 21:04:23 +0100

    added get companions query

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -1,6 +1,8 @@
 "use server";
 import { auth } from "@clerk/nextjs/server";
 import { createSupabaseClient } from "../supabase";
+import { createCompanion } from "@/lib/actions/companion.actions";
+
 export const createCompanion = async (formData: CreateCompanion) => {
   const { userId: author } = await auth();
   const supabase = createSupabaseClient();
@@ -14,4 +16,19 @@ export const createCompanion = async (formData: CreateCompanion) => {
     throw new Error(error?.message || "Failed to create a companion");
 
   return data[0];
+};
+
+export const getAllCompanions = async ({
+  limit = 10,
+  page = 1,
+  subject,
+  topic,
+}: GetAllCompanions) => {
+  const supabase = createSupabaseClient();
+  let query = supabase.from("companions").select();
+  if (subject && topic) {
+    query = query
+      .ilike("subject", `%${subject}%`)
+      .or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
+  }
 };
