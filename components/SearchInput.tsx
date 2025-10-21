c7ddbfa18d85f2bef9c24dd6ca44d7d8c58f39e3
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-10-21 18:29:23 +0100

    added the search companion input field

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+"use client";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { useState } from "react";
+import Image from "next/image";
 const SearchInput = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParam = useSearchParams();
+  const query = searchParam.get("topic") || "";
+  const [searchQuery, setSearchQuery] = useState("");
   return (
-    <div>SearchInput</div>
-  )
-}
+    <div className="relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit">
+      <Image src="/icons/search.svg" alt="search" width={15} height={15} />
+      <input
+        placeholder="Search companions..."
+        className="outline-none"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+    </div>
+  );
+};
 
-export default SearchInput
+export default SearchInput;
