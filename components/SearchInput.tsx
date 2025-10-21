cbb09204099535f4d85d958a8e8d661644fa42a2
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-10-21 18:51:11 +0100

    added js mastery utils

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,13 +1,19 @@
 "use client";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 const SearchInput = () => {
   const pathname = usePathname();
   const router = useRouter();
   const searchParam = useSearchParams();
   const query = searchParam.get("topic") || "";
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState(query);
+  useEffect(() => {
+    if (searchQuery) {
+      // navigate to the current pathname with the topic query param
+      router.push(`/currentRoute?topic=${searchQuery}`);
+    }
+  }, [searchQuery, router, pathname, searchParam]);
   return (
     <div className="relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit">
       <Image src="/icons/search.svg" alt="search" width={15} height={15} />
