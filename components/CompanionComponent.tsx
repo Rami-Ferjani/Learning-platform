4ca11e1404fd480287342edd1f967d04c32dffbf
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-11-20 22:20:18 +0100

    added state to companion component

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
-
-const CompanionComponent = () => {
+import React, { useState } from "react";
+import {cn,getSubjectColor} from "@/lib/utils";
+ interface CompanionComponentProps {
+        companionId: string;
+        subject: string;
+        topic?: string;
+        name?: string;
+        userName?: string;
+        userImage?: string;
+        style?: React.CSSProperties;
+        voice?: string;
+    }
+const CompanionComponent = ({ companionId,subject,topic,name,userName,userImage,style,voice} :CompanionComponentProps}) => {
+  const [callStatus,setCallStatus]=useState<CallStatus>(CallStatus.inactive);
   return (
-    <div>CompanionComponent</div>
-  )
-}
+    <section className="flex flex-col h-[70vh]">
+      <section className="flex gap-8 max-sm:flex-col">
+        <div className="companion-section">
+          <div
+            className="companion-avatar"
+            style={{ backgroundColor: getSubjectColor(subject) }}
+          >
+            <div className={cn('absolute transition-opacity duration-1000')}></div>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+};
 
-export default CompanionComponent
+export default CompanionComponent;
