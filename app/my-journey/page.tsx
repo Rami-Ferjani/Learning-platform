dd04f81b34e1553d77e8b4752684a180a26a3064
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-12-28 18:31:38 +0100

    retrieves the user sessions data in the profile page

--- app/my-journey/page.tsx
+++ app/my-journey/page.tsx
@@ -5,7 +5,15 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-const Profile = () => {
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { getUserCompanions, getUserSessions } from "@/lib/actions/companion.actions";
+const Profile = async () => {
+  const user = await currentUser();
+  if (!user) redirect("/sign-in");
+  const companions=await getUserCompanions(user.id);
+  const sessionHistory=await getUserSessions(user.id);
+
   return (
     <main className="min-lg:w-3/4">
       <section className="">
