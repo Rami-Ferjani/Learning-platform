265e2f86b60f5f32eb71a98a873ab0f642543e70
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-12-31 16:51:21 +0100

    Ads restriction to creating companions for each plan

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,17 +1,44 @@
 import CompanionsForm from "@/components/CompanionsForm";
+import { NewCompanionPermission } from "@/lib/actions/companion.actions";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
 const NewCompanion = async () => {
   const { userId } = await auth();
   if (!userId) redirect("/sign-in");
+  const canCreateCompanion = await NewCompanionPermission(userId);
 
   return (
     <main className="main-lg:w-1/3 min-md:w-2/3 items-center justify-center">
-      <article className="w-full gap-4 flex flex-col">
-        <h1>
-          <CompanionsForm />
-        </h1>
-      </article>
+      {canCreateCompanion ? (
+        <article className="w-full gap-4 flex flex-col">
+          <h1>
+            <CompanionsForm />
+          </h1>
+        </article>
+      ) : (
+        <article className="companion-limit">
+          <Image
+            src="/images/limit.svg"
+            alt="Companion limit reached"
+            width={360}
+            height={360}
+          />
+          <div className="cta-badge">Upgrade your plan</div>
+          <h1>You've Reached your Limit</h1>
+          <p>
+            You've reached your companion Limit.upgrade to create more
+            companions and premium features.
+          </p>
+          <Link
+            href="/subscription"
+            className="btn-primary w-full justify-center"
+          >
+            Upgrade my PLan
+          </Link>
+        </article>
+      )}
     </main>
   );
 };
--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -1,6 +1,8 @@
 "use server";
 import { auth } from "@clerk/nextjs/server";
 import { createSupabaseClient } from "../supabase";
+import NewCompanion from "./../../app/companions/new/page";
+import { supabaseIntegration } from "@sentry/nextjs";
 
 export const createCompanion = async (formData: CreateCompanion) => {
   const { userId: author } = await auth();
@@ -95,4 +97,25 @@ export const getUserCompanions = async (userId: string) => {
 
   if (error) throw new Error(error.message);
   return data;
+};
+
+export const NewCompanionPermission = async (userId: string) => {
+  const { user, has } = await auth();
+  const supabase = createSupabaseClient();
+  let limit = 0;
+  if (has({ plan: "pro" })) {
+    return true;
+  } else if (has({ feature: "3_active_companions" })) {
+    limit = 3;
+  } else if (has({ feature: "10_active_companions" })) {
+    limit = 10;
+  }
+  const { data, error } = await supabase
+    .from("Companions")
+    .select("id", { count: "exact" })
+    .eq("author", userId);
+  if (error) throw new Error(error.message);
+  const companionCount = data?.length;
+  if (companionCount >= limit) return false;
+  else return true;
 };
